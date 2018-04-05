572803c9bad8c7d0b5bdc9681f6040752ff4e028
kevin viglucci <kviglucci@gmail.com>
2018-04-05 00:03:04 -0500

    consume RouterFactory

--- server.js
+++ server.js
@@ -1,20 +1,45 @@
 const express = require('express');
 const names = require('starwars-names');
-const framework = require('framework-x'); // linked locally from https://github.com/viglucci/framework-x
+const framework = require('framework-x'); // using npm link
 const Router = framework.http.Router
+const RouterFactory = framework.http.ExpressRouterFactory
 
 const app = express();
 const router = new Router();
 
+router.middleware((req, res, next) => {
+    console.log('global middleware', req.url);
+    next();
+});
+
+router.get('/', (req, res) => {
+    res.send('hello world');
+});
+
 router.group('/api', (apiRouter) => {
+
     apiRouter.group('/v1', (apiV1Router) => {
+        apiV1Router.middleware((req, res, next) => {
+            console.log('v1 middleware', req.url);
+            next();
+        });
         apiV1Router.get('/names', (req, res) => {
+            res.json(names.all);
+        });
+    });
+
+    apiRouter.group('/v2', (apiV2Router) => {
+        apiV2Router.middleware((req, res, next) => {
+            console.log('v2 middleware', req.url);
+            next();
+        });
+        apiV2Router.get('/names', (req, res) => {
             res.json(names.all);
         });
     });
 });
 
-app.use(router.bind(express.Router()));
+app.use(RouterFactory.create(router));
 
 const port = 3000;
 app.listen(port, () => {
